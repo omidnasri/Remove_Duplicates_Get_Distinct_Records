1649a1d855d8d473f858f559ea7e1fb39eebb061
Omid Nasri <omid.nasri@outlook.com>
2015-12-15 19:20:57 +0330

    Add example 2.

--- Remove_Duplicates_Get_Distinct_Records/Program.cs
+++ Remove_Duplicates_Get_Distinct_Records/Program.cs
@@ -21,6 +21,20 @@ namespace Remove_Duplicates_Get_Distinct_Records
                 foreach (var item in DistinctItems)
                     Console.WriteLine(item.Name);
             }
+            // Example 2:
+            {
+                List<Employee> employees2 = new List<Employee>(){
+                     new Employee{EmpID =1,Name="1"}
+                   , new Employee{EmpID =2,Name="3"}
+                   , new Employee{EmpID =3,Name="3"}
+                   , new Employee{EmpID =4,Name="4"}
+                   , new Employee{EmpID =5,Name="6"}
+                   , new Employee{EmpID =6,Name="6"}
+                };
+                List<Employee> duplicateEmployees = employees2.Except(employees2.GroupBy(i => i.Name).Select(ss => ss.First())).ToList();
+                foreach (var item in duplicateEmployees)
+                    Console.WriteLine("\t" + item.Name);
+            }
             Console.ReadLine();
         }
     }
